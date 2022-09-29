28e1b068c9615939b6198792ec13fbff4beef828
Jani Hyvonen <jani.k.hyvonen@gmail.com>
2022-09-29 20:10:50 +0300

    test: host blinding demo vol2
    Now working
    
    Signed-off-by: Jani Hyvonen <jani.k.hyvonen@gmail.com>

--- test/blinding/demo/guest/demo-guest.c
+++ test/blinding/demo/guest/demo-guest.c
@@ -11,39 +11,28 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("janikh");
 MODULE_DESCRIPTION("Guest side kvms crosvm demo module");
 
-extern uintptr_t	_binary_image_png_start;
-extern uintptr_t	_binary_image_png_size;
-extern uintptr_t	_binary_image_png_end;
+extern uintptr_t	_binary_secret_png_start;
+extern uintptr_t	_binary_secret_png_end;
 
-#define IMAGE_BASE	((unsigned long)(&_binary_image_png_start))
-#define IMAGE_SIZE	((unsigned long)(&_binary_image_png_size))
-#define IMAGE_END	((unsigned long)(&_binary_image_png_end))
+#define IMAGE_BASE	((unsigned long)(&_binary_secret_png_start))
+#define IMAGE_END	((unsigned long)(&_binary_secret_png_end))
 
 static int __init demo_entry(void)
 {
-	uint64_t phys, phye;
+	uint64_t phys, virts = IMAGE_BASE;
+	size_t image_size = IMAGE_END - IMAGE_BASE;
 
 	pr_info("%s %s...->\n", __FILE__, __func__);
-	pr_info("    Image start virtual: 0x%lx\n", IMAGE_BASE);
-	pr_info("    Image end virtual: 0x%lx\n", IMAGE_END);
+	pr_info("    Secret start virtual: 0x%llx\n", virts);
+	pr_info("    Secret end virtual: 0x%lx\n", IMAGE_END);
+	pr_info("    Secret size: 0x%lx dec:%ld\n", image_size, image_size);
 
-	phys = (uint64_t)virt_to_phys((void *)IMAGE_BASE);
-	phye = (uint64_t)virt_to_phys((void *)IMAGE_END);
-
-	pr_info("    Image start physical: 0x%llx\n", phys);
-	pr_info("    Image end physical: 0x%llx\n", phye);
-
-	phys = (uint64_t)virt_to_ipa(IMAGE_BASE);
-	phye = (uint64_t)virt_to_ipa(IMAGE_END);
-
-	pr_info("    Image start ipa: 0x%llx\n", phys);
-	pr_info("    Image end ipa: 0x%llx\n", phye);
-
-	phys = kvms_hyp_call(HYP_TRANSLATE, phys);
-	phye = kvms_hyp_call(HYP_TRANSLATE, phye);
-
-	pr_info("    Image start phy: 0x%llx\n", phys);
-	pr_info("    Image end phy: 0x%llx\n", phye);
+	for (virts = IMAGE_BASE; virts < (IMAGE_BASE + image_size); virts += PAGE_SIZE) {
+		phys = (uint64_t)virt_to_ipa(virts);
+		pr_info("    Secret page ipa: 0x%llx\n", phys);
+		phys = kvms_hyp_get(HYP_TRANSLATE, phys);
+		pr_info("    Secret page phy: 0x%llx\n", phys);
+	}
 
 	return 0;
 }
--- test/blinding/demo/host/demo-host.c
+++ test/blinding/demo/host/demo-host.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/mm.h>
 
 #include "../arch/arm64/kvm/hvccall-defines.h"
 
@@ -17,6 +18,7 @@
 // rmmod demo-h.ko
 
 #define NAME "kvms_host_side_demo"
+#define MAX_ADDR 16
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("janikh");
@@ -25,60 +27,77 @@ MODULE_DESCRIPTION("Host side kvms crosvm demo module");
 static int major;
 uint64_t read_paddr;
 uint64_t read_size;
-char kbuf[128];
+char kbuf[PAGE_SIZE * MAX_ADDR];
+uint64_t addrs[MAX_ADDR];
 
 static ssize_t device_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
 {
 	size_t ret = 0;
-	size_t rlen = sizeof(kbuf);
+	if (*off || len > (PAGE_SIZE * MAX_ADDR))
+		return ret;
 
-	if (*off == 0) {
-		sprintf(kbuf, "read from: %llx len: %lld\n", read_paddr, read_size);
-		if (copy_to_user(buf, kbuf, rlen)) {
-			ret = -EFAULT;
-		} else {
-			ret = rlen;
-			*off = 1;
-		}
+	ret = copy_to_user((void *)buf, (void *)kbuf, len);
+	if (ret) {
+		pr_info("copy_to_user error %ld\n", ret);
+		ret = -EFAULT;
+	} else {
+		pr_info("copy_to_user %ld bytes\n", len);
+		ret = len;
+		*off = 0;
 	}
+
 	return ret;
 }
 
 static ssize_t device_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
 	size_t ret = 0;
+	char *kbufptr = kbuf;
 	size_t wlen = sizeof(kbuf);
-	int cmd;
+	char *argument;
+	const char *delim = " ";
+	struct page *page;
+	void *vaddr;
+	int i;
+	char *check;
+	size_t rlen = 0;
 
 	if (len < wlen)
 		wlen = len;
 
-	memset(kbuf, 0, sizeof(kbuf));
+	memset(addrs, 0, sizeof(addrs));
 
-	if (*off == 0) {
-		if (copy_from_user(kbuf, buf, wlen)) {
-			ret = -EFAULT;
-		} else {
-			if (kstrtoull(kbuf, 16, &read_size)) {
-				pr_info("    only hex values allowed: %s\n", kbuf);
-				cmd = 0;
-			}
-			pr_info("    command: %d\n", cmd);
-			switch (cmd) {
-			case 1:
-			case 2:
-			case 3:
-				ret = __kvms_hvc_cmd(HYP_HOST_BOOTSTEP, cmd);
-				if (ret)
-					pr_err("kvm: %s failed: %ld\n", __func__, ret);
-				break;
-			default:
-				break;
-			}
-			ret = wlen;
-			*off = 1;
+	if (*off)
+		return ret;
+
+	if (copy_from_user(kbuf, buf, wlen))
+		return -EFAULT;
+
+	for (i = 0; i < MAX_ADDR; i++) {
+		argument = strsep(&kbufptr, delim);
+		if (argument == NULL)
+			break;
+		if (kstrtoull(argument, 16, &addrs[i])) {
+			pr_err("64bit hex values only: %s\n", argument);
+			addrs[0] = 0;
+			break;
 		}
 	}
+
+	for (i = 0; i < MAX_ADDR; i++) {
+		if (!addrs[i])
+			break;
+		page = phys_to_page(addrs[i]);
+		vaddr = page_address(page);
+		check = (char *)vaddr;
+		pr_info("read from: %llx len: 0x%lx\n", (u_int64_t)vaddr, PAGE_SIZE);
+		pr_info("vaddr %x%x%x%x%x%x%x%x\n", check[0], check[1], check[2], check[3], check[4], check[5], check[6], check[7]);
+		memcpy((void *)&kbuf[rlen], vaddr, PAGE_SIZE);
+		rlen += PAGE_SIZE;
+	}
+
+	ret = wlen;
+	*off = 1;
 	return ret;
 }
 
