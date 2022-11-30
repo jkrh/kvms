6a8e79ba4ce86adec8cbd851ba444bd463b7d842
Janne Karhunen <Janne.Karhunen@gmail.com>
2022-11-30 17:12:13 +0200

    stdlib: fix missing stdlib/lz4 license
    Signed-off-by: Janne Karhunen <Janne.Karhunen@gmail.com>

--- stdlib/lz4.h
+++ stdlib/lz4.h
@@ -1,7 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __LZ4_H__
+#define __LZ4_H__
+
 #include <stdint.h>
 
-#ifndef UNSAFE_LZ4
+#ifdef UNSAFE_LZ4
 uint64_t lz4dec(const void *src, void *dst, uint64_t srcsz);
 #else
 uint64_t lz4dec(const void *src, void *dst, uint64_t srcsz, uint64_t dstsz);
+#endif
+
 #endif
