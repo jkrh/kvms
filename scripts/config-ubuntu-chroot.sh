4640c3c21bdc176a1ce88fe17d26d6dc28c947b1
Jani Hyvonen <jani.hyvonen@digital14.com>
2022-12-09 14:20:59 +0200

    scripts: add spice socket visibility from chroot
    + Add environment variable for SELinux mode control
    + Add bind mount for mnt folder
    
    Signed-off-by: Jani Hyvonen <jani.hyvonen@digital14.com>

--- scripts/config-ubuntu-chroot.sh
+++ scripts/config-ubuntu-chroot.sh
@@ -1,5 +1,11 @@
 #!/system/bin/sh
 
+if [ -n "$ENFORCE" ]; then
+	setenforce $ENFORCE
+fi
+ENFORCE=$(getenforce)
+echo "Enforce == $ENFORCE"
+
 mkdir -p /data/vmapps
 
 [ ! -z $1 ] && tar xf $1 -C /data/vmapps
@@ -11,6 +17,6 @@ set -e
 # Due to CONFIG_ANDROID_PARANOID_NETWORK
 echo 'APT::Sandbox::User "root";' > $CHROOTDIR/etc/apt/apt.conf.d/01-android-nosandbox
 echo "nameserver 8.8.8.8" > $CHROOTDIR/etc/resolv.conf
-for MNT in proc sys dev; do mount --rbind /$MNT $CHROOTDIR/$MNT; done
+for MNT in proc sys dev mnt; do mount --rbind /$MNT $CHROOTDIR/$MNT; done
 
 PATH=/usr/bin:/usr/sbin /system/bin/chroot /data/vmapps/ubuntu /bin/bash
